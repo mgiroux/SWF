a8fd50a2757c1edc503823ecd0faf973b135775b
Marc Giroux <marcgiroux@hotmail.com>
2018-05-26 10:53:04 -0400

    fixes to package

--- Package.swift
+++ Package.swift
@@ -1,14 +1,12 @@
 import PackageDescription
+
 let package = Package(
 	name: "SwiftWebFramework",
-	products: [
-    		.library(name: "SWiftWebFramework", targets: ["SWF"]),
-  	],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
-        	.Package(url: "https://github.com/SwiftORM/MongoDB-StORM.git", majorVersion: 3),
-        	.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3),
-        	.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 3),
-        	.Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 11)
+        .Package(url: "https://github.com/SwiftORM/MongoDB-StORM.git", majorVersion: 3),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 3),
+        .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 11)
 	]
 )
